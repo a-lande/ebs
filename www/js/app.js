6705c0e1ea8c4be3c16ce8d56673b23c2c420fed
dana raviv <dana.nissan@gmail.com>
2015-10-11 19:24:40 +0300

    restoring ionic-material

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('ebsApp', ['ionic', 'ebs.controllers', 'ap.services', 'ngAnimate'])
+angular.module('ebsApp', ['ionic','ionic-material', 'ebs.controllers', 'ap.services', 'ngAnimate', ])
 
 .run(function ($rootScope, $ionicPlatform, $http, apWebService, AuthService, $state) {
     $rootScope.appId = 'apebs';
