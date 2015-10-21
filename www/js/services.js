638ee3ec659ab81b400a15f854f0e84618df80ed
a-lande <ariel.lande@gmail.com>
2015-10-21 15:11:11 +0300

    Fixing Login issue in older devices

--- www/js/services.js
+++ www/js/services.js
@@ -299,4 +299,11 @@
       }
     }
   }]);
+  // polyfills
+  if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function (searchString, position) {
+      position = position || 0;
+      return this.indexOf(searchString, position) === position;
+    };
+  }
 })();
